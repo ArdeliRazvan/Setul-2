299e1f236e753b97793f1685c77b558030b67b76
ArdeliRazvan <146825367+ArdeliRazvan@users.noreply.github.com>
2024-01-18 17:12:39 +0200

    Add files via upload

---
+++ Problema 14/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introdu lungimea secventei: ");
+        int lung = int.Parse(Console.ReadLine());
+
+        if (lung < 2)
+        {
+            Console.WriteLine($"Secventa este o secventa monotonă rotită: true");
+            return;
+        }
+
+        Console.Write("Introdu elementul 1: ");
+        int primulNumar = int.Parse(Console.ReadLine());
+        int ultimulNumar = primulNumar;
+        int numarCurent = 0;
+        bool aAvutLocRotire = false;
+
+        for (int i = 1; i < lung; i++)
+        {
+            Console.Write($"Introdu elementul {i + 1}: ");
+            numarCurent = int.Parse(Console.ReadLine());
+
+            if (ultimulNumar != numarCurent)
+            {
+                aAvutLocRotire = true;
+            }
+
+            ultimulNumar = numarCurent;
+        }
+
+        // Verificăm dacă secvența poate fi ordonată monoton prin rotiri succesive
+        if (!aAvutLocRotire || (aAvutLocRotire && (numarCurent >= primulNumar || numarCurent <= ultimulNumar)))
+        {
+            Console.WriteLine($"Secventa este o secventa monotonă rotită: true");
+        }
+        else
+        {
+            Console.WriteLine($"Secventa nu este o secventa monotonă rotită");
+        }
+    }
+}
---
+++ Problema 15/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introdu lungimea secventei: ");
+        int lung = int.Parse(Console.ReadLine());
+
+        if (lung < 3)
+        {
+            Console.WriteLine("Secventa este bitonica: false");
+            return;
+        }
+
+        Console.Write("Introdu elementul 1: ");
+        int primulNumar = int.Parse(Console.ReadLine());
+        int numarCurent = 0;
+        bool aInceputCrescator = false;
+        bool aInceputDescrescator = false;
+
+        for (int i = 1; i < lung; i++)
+        {
+            Console.Write($"Introdu elementul {i + 1}: ");
+            numarCurent = int.Parse(Console.ReadLine());
+
+            if (numarCurent > primulNumar)
+            {
+                aInceputCrescator = true;
+            }
+            else if (numarCurent < primulNumar)
+            {
+                aInceputDescrescator = true;
+            }
+
+            // Verificăm dacă ambele condiții pentru o secvență bitonică sunt îndeplinite
+            if (aInceputCrescator && aInceputDescrescator)
+            {
+                Console.WriteLine("Secventa este bitonica: true");
+                return;
+            }
+
+            primulNumar = numarCurent;
+        }
+
+        Console.WriteLine("Secventa este bitonica: false");
+    }
+}
